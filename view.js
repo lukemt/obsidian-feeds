ec44e1105f85ec1a3598650ef827e9c1e1484cfa
François Vaux <madx+github@yapok.org>
2023-10-17 17:09:07 +0200

    💄 Improve Settings panel trigger for Live Preview

--- view.js
+++ view.js
@@ -15,6 +15,8 @@ const config = {
   showTree: false,
 };
 
+const lucideGear = `<div class="feed-settings-button" aria-label="Feed settings"><svg xmlns="http://www.w3.org/2000/svg" class="svg-icon lucide-settings" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-settings"><path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"/><circle cx="12" cy="12" r="3"/></svg></div>`;
+
 const fileName = dv.current()?.file?.name;
 const filePath = dv.current()?.file?.path;
 if (fileName == null) dv.el("div", "Please reopen the file to show the feed");
@@ -54,19 +56,17 @@ const setStateProperty = (propName, value, refresh = true) => {
 
 const addShowOptionsLink = () => {
   const link = document.createElement("a");
+  link.innerHTML = lucideGear;
 
   if (state.showOptionsPanel) {
-    link.textContent = "< Hide options";
     link.onclick = () => {
       setStateProperty("showOptionsPanel", false);
     };
   } else {
-    link.textContent = "Show options >";
     link.onclick = () => {
       setStateProperty("showOptionsPanel", true);
     };
   }
-  link.style.margin = "0.2em";
   if (state.showOptions) {
     // don't showOptions panel at all and configure via code
     configEl.appendChild(link);
@@ -142,7 +142,6 @@ const addCopyFeedButton = (result) => {
 
 addShowOptionsLink();
 if (state.showOptionsPanel) {
-  addNewLine();
   addSelect("Only with tasks", "onlyWithTasks", ["", "any", "undone", "done"]);
   addNewLine();
   addToggle("Show tree", "showTree");
